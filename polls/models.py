98038647a146fcff888ff2c2e38ad2ff070c96c3
Danilo Oliveira <dohkoweeds74@gmail.com>
2021-04-14 13:57:36 -0400

    correcoes de estilo de codigo

--- polls/models.py
+++ polls/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def was_published_recently(self):
-    
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
@@ -21,6 +21,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def _str_(self):
-    
         return self.choice_text
